43e1ab471da8440b8ae70ff15083346d1088dae7
LloydHawkeye <nicolas.lange33@gmail.com>
2024-04-15 20:20:32 +0200

    Release 1.0.0

---
+++ WolfCanTakeItem/Patch/PlayerControllerPatch.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Fusion;
+using Managers;
+using UnityEngine;
+using static PlayerController;
+
+namespace WolfCanTakeItem.Patch
+{
+    internal class PlayerControllerPatch
+    {
+        public static void Patch() 
+        {
+            On.PlayerController.CheckItemRayCast += PlayerController_CheckItemRayCast;
+            On.PlayerController.UseItem += PlayerController_UseItem;
+        }
+
+        private static void PlayerController_UseItem(On.PlayerController.orig_UseItem orig, PlayerController self)
+        {
+            if (!GameManager.LightingManager.IsTransition && GameManager.State.Current == GameState.EGameState.Play && self.Item != null && !self.IsClimbing && (bool)self.CanMoveAnimation)
+            {
+                self.Item.Use();
+            }
+        }
+
+        private static bool PlayerController_CheckItemRayCast(On.PlayerController.orig_CheckItemRayCast orig, PlayerController self, Item targetItem)
+        {
+            if (targetItem.Owner == PlayerRef.None)
+            {
+                GameManager.Instance.gameUI.UpdateInteraction("UI_LOOT_ITEM", Color.white, InputActionName.None);
+                return true;
+            }
+            return false;
+        }
+    }
+}
